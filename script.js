3b2d6e9581c133a9909c0952d69a08d0cf949de2
Klem_6 <134615113+MEROU-Clemence@users.noreply.github.com>
2026-02-27 16:07:30 +0100

    start header nav

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // ****** Nav
+    // Mobile Nav
+    var MobNav = $('.navbar-toggler');
+    MobNav.on('click', function () {
+        $('.navbar-toggler .btn-menu').toggleClass('d-none');
+        $('.nav-mobile').toggleClass('nav-mobile-active');
+    });
 
+    var ClickMenu = $('.nav-mobile ul li');
+    ClickMenu.on('click', function() {
+        $('.navbar-toggler .btn-menu').toggleClass('d-none');
+        $('.nav-mobile').toggleClass('nav-mobile-active');
+    })
 })
