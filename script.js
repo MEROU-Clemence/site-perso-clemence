c1681c929c8c6fe67f3bc45867bab0b2ee49fafe
Klem_6 <134615113+MEROU-Clemence@users.noreply.github.com>
2026-03-14 11:38:11 +0100

    start effet cursor

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // ****** Nav
-    // Mobile Nav
-    var MobNav = $('.navbar-toggler');
-    MobNav.on('click', function () {
-        $('.navbar-toggler .btn-menu').toggleClass('d-none');
-        $('.nav-mobile').toggleClass('nav-mobile-active');
-        $('.first-nav').toggleClass('first-nav-active');
-    });
+    // ****** Nav Mobile
+    function openBurger() {
+        var MobNav = $('.navbar-toggler');
+        MobNav.on('click', function () {
+            $('.navbar-toggler .btn-menu').toggleClass('d-none');
+            $('.nav-mobile').toggleClass('nav-mobile-active');
+            $('.first-nav').toggleClass('first-nav-active');
+        });
+    }
 
-    var ClickMenu = $('.nav-mobile ul li');
-    ClickMenu.on('click', function() {
-        $('.navbar-toggler .btn-menu').toggleClass('d-none');
-        $('.nav-mobile').toggleClass('nav-mobile-active');
-        $('.first-nav').toggleClass('first-nav-active');
-    })
+    // ****** Curseur animation
+    function cursorFollow() {
+        const cursor = document.querySelector(".cursor-follow");
+
+        let mouseX = 0;
+        let mouseY = 0;
+        let posX = 0;
+        let posY = 0;
+
+        let started = false;
+        let hideTimer;
+
+        document.addEventListener("mousemove", (e) => {
+
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+
+        if(!started){
+            posX = mouseX;
+            posY = mouseY;
+            started = true;
+        }
+
+        cursor.style.opacity = "1";
+
+        clearTimeout(hideTimer);
+
+        hideTimer = setTimeout(() => {
+            cursor.style.opacity = "0";
+        }, 300);
+
+        });
+
+        function animate(){
+
+        if(started){
+            posX += (mouseX - posX) * 0.1;
+            posY += (mouseY - posY) * 0.1;
+
+            cursor.style.left = posX + "px";
+            cursor.style.top = posY + "px";
+        }
+
+        requestAnimationFrame(animate);
+        }
+
+        animate();
+    }
+
+    // ****** Initialisation des modules
+    openBurger();
+    cursorFollow();
 })
